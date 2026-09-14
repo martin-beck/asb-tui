405ae898577e3b37c1082cbaf0e64763d8286645
Martin Beck <martin.beck2@gmx.de>
2026-09-14 17:23:23 +0200

    fix: add publication header to selection tests
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/selection.rs
+++ tests/selection.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+// SPDX-License-Identifier: MIT
+
 use asb_tui::selection::{GroupSelection, Measurement, MeasurementSelection, SelectionError};
 
 fn catalog() -> MeasurementSelection {
