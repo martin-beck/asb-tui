b36555e768ab44eccbaae88ab8d6769019b8612a
Martin Beck <martin.beck2@gmx.de>
2026-09-18 14:56:43 +0200

    chore(wizard): document completion mutation state
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tools/test-ui-state-model.py
+++ tools/test-ui-state-model.py
@@ -68,6 +68,9 @@ class UiStateModelTests(unittest.TestCase):
     def test_wizard_catalog_state_is_formally_declared(self):
         self.assertIn("wizard_catalog", self.model["state_fields"])
 
+    def test_wizard_completion_state_is_formally_declared(self):
+        self.assertIn("wizard_completion", self.model["state_fields"])
+
     def test_wizard_editing_help_and_completion_bindings_are_formally_declared(self):
         bindings = {(item["action"], item["element"], item["key"]) for item in self.model["bindings"]}
         self.assertIn(("wizard_edit", "wizard.agent", "Character"), bindings)
